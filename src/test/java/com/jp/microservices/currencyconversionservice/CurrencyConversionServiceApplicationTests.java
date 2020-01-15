751f98461961c9496fd02695611a1489ab002f2b
juan <oqundo.juan@system-in-motion.com>
2020-01-15 17:52:23 +0800

    first commit

---
+++ src/test/java/com/jp/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.microservices.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

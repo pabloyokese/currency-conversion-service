ab7e67247c284207cbbdc857966aac378bbc56cd
juan <oqundo.juan@system-in-motion.com>
2020-01-20 13:28:36 +0800

    adding slueth

--- src/main/java/com/jp/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/jp/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,5 +1,7 @@
 package com.jp.microservices.currencyconversionservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import java.util.Map;
 public class CurrencyConversionController {
     @Autowired
     private CurrencyExchangeServiceProxy proxy;
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean convertCurrency(@PathVariable("from") String from,
@@ -39,6 +42,7 @@ public class CurrencyConversionController {
                                                   @PathVariable("to") String to,
                                                   @PathVariable("quantity") BigDecimal quantity) {
         CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+        logger.info("{}", response);
         return new CurrencyConversionBean(response.getId(), from, to,
                 response.getConversionMultiple(),
                 quantity,
